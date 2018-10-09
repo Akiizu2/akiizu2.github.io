315287a4781245e2465b2b9115d35f03d0310b2d
Vitaya jealwarakun <vitaya.j@scale360solutions.com>
2018-10-09 20:16:42 +0700

    Initial Page

---
+++ precache-manifest.9fd1aa099c4074c12bdf0dbcfccb9226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77d33cc41aff0690e99d96a72565b0e7",
+    "url": "/static/media/my_photo.77d33cc4.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13776dd394e4b7c9e808",
+    "url": "/static/js/main.13776dd3.chunk.js"
+  },
+  {
+    "revision": "c99f717d73b9a7cb0883",
+    "url": "/static/js/1.c99f717d.chunk.js"
+  },
+  {
+    "revision": "13776dd394e4b7c9e808",
+    "url": "/static/css/main.7ed9723b.chunk.css"
+  },
+  {
+    "revision": "fae674b4efed574fe452f122882413e6",
+    "url": "/index.html"
+  }
+];
