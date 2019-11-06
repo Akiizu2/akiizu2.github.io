bc5e8dafa948e658366cea7f2bbb4aff66835c00
Vitaya Jealwarakun <vitaya.j@lqid360.com>
2019-11-06 21:40:48 +0700

    Update CV

---
+++ precache-manifest.282bb18e72ed5d9d1dc90f5016d96eaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6a7d80ddd3f37ade8a46346c534a95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa0ad7998f820c1b7264",
+    "url": "/static/css/main.c8a8a76b.chunk.css"
+  },
+  {
+    "revision": "3d4ace0f3b05f384551e",
+    "url": "/static/js/2.f06b1278.chunk.js"
+  },
+  {
+    "revision": "fa0ad7998f820c1b7264",
+    "url": "/static/js/main.658995f6.chunk.js"
+  },
+  {
+    "revision": "480c1fd3b5e8c4fddde2",
+    "url": "/static/js/runtime~main.25c70671.js"
+  }
+]);
