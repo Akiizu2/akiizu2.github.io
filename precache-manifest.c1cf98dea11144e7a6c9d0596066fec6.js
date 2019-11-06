2636e5738973ced3688160d7f92b79d780974c54
Vitaya Jealwarakun <vitaya.j@lqid360.com>
2019-11-06 21:31:05 +0700

    Update Page

---
+++ precache-manifest.c1cf98dea11144e7a6c9d0596066fec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e37e318dd79819d288431afc1dbe336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eab2613ca43a5e1f7a6c",
+    "url": "/static/css/main.c95a5e05.chunk.css"
+  },
+  {
+    "revision": "3d4ace0f3b05f384551e",
+    "url": "/static/js/2.f06b1278.chunk.js"
+  },
+  {
+    "revision": "eab2613ca43a5e1f7a6c",
+    "url": "/static/js/main.af95cfc4.chunk.js"
+  },
+  {
+    "revision": "480c1fd3b5e8c4fddde2",
+    "url": "/static/js/runtime~main.25c70671.js"
+  }
+]);
