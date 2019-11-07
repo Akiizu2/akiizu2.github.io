d29c687857a837a8e55c151f9212ab699c7d80df
Vitaya Jealwarakun <vitaya.j@lqid360.com>
2019-11-07 10:46:51 +0700

    Change profile picture

--- precache-manifest.2b45ce92be466ca3dcfe87156bfb695c.js
+++ precache-manifest.2b45ce92be466ca3dcfe87156bfb695c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d79a08a314264ed68222037d746759e9",
+    "revision": "a9f5609272bdcd266d47aa2bbc32fed1",
     "url": "/index.html"
   },
   {
-    "revision": "94caffc2994482ded685",
-    "url": "/static/css/main.b29db341.chunk.css"
+    "revision": "cc670ca7ce55d4493997",
+    "url": "/static/css/main.4a3222ec.chunk.css"
   },
   {
     "revision": "e321d4c9d2f557e08b6a",
     "url": "/static/js/2.8d9d0028.chunk.js"
   },
   {
-    "revision": "94caffc2994482ded685",
-    "url": "/static/js/main.08f69792.chunk.js"
+    "revision": "cc670ca7ce55d4493997",
+    "url": "/static/js/main.878811f7.chunk.js"
   },
   {
     "revision": "480c1fd3b5e8c4fddde2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b66849cd13fa1a88ed406c218a0660b9.js"
+  "/precache-manifest.2b45ce92be466ca3dcfe87156bfb695c.js"
 );
 
 self.addEventListener('message', (event) => {
